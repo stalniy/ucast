575efc9fbe55e8bf235423a365abed5147e6dd39
Sergii <sergiy.stotskiy@gmail.com>
2020-08-13 14:57:45 +0300

    fix(builder): renames condition builder methods as they very likely to conflict with interpreter

--- packages/core/src/builder.ts
+++ packages/core/src/builder.ts
@@ -1,5 +1,5 @@
 import { Condition } from './Condition';
 import { optimizedCompoundCondition } from './utils';
 
-export const and = (conditions: Condition[]) => optimizedCompoundCondition('and', conditions);
-export const or = (conditions: Condition[]) => optimizedCompoundCondition('or', conditions);
+export const buildAnd = (conditions: Condition[]) => optimizedCompoundCondition('and', conditions);
+export const buildOr = (conditions: Condition[]) => optimizedCompoundCondition('or', conditions);
