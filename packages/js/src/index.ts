1cf3a0ede0de4bff490ebb4045b85e701a85be8c
Jérémy <jeremy.huard@doeo.fr>
2020-08-22 22:57:48 +0200

    refactor(js/index): remove projected-field export as it's not used anymore

--- packages/js/src/index.ts
+++ packages/js/src/index.ts
@@ -1,5 +1,4 @@
 export * from './interpreters';
 export * from './interpreter';
 export * from './defaults';
-export { PROJECTED_FIELD } from './utils';
 export type { JsInterpretationOptions, JsInterpreter } from './types';
